57d1e0639b263062a680746fef01dde4174b5b4d
Nikolay Bryskin <nikicat@users.noreply.github.com>
2024-11-22 16:09:33 +0300

    l2 compatibility: remove v,r,s checks

--- core/types/transaction.go
+++ core/types/transaction.go
@@ -26,7 +26,6 @@ import (
 	"time"
 
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/rlp"
 )
 
@@ -236,7 +235,7 @@ func sanityCheckSignature(v *big.Int, r *big.Int, s *big.Int, maybeProtected boo
 		return ErrUnexpectedProtection
 	}
 
-	var plainV byte
+	/*var plainV byte
 	if isProtectedV(v) {
 		chainID := deriveChainId(v).Uint64()
 		plainV = byte(v.Uint64() - 35 - 2*chainID)
@@ -252,7 +251,7 @@ func sanityCheckSignature(v *big.Int, r *big.Int, s *big.Int, maybeProtected boo
 	}
 	if !crypto.ValidateSignatureValues(plainV, r, s, false) {
 		return ErrInvalidSig
-	}
+	}*/
 
 	return nil
 }
